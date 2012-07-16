6691fa061fc7592b2fa11f3b2a18f340dbf060c6
Michael Cohoon <MichaelCohoon@gmail.com>
2012-07-15 21:44:14 -0400

    Wrote methods to read/write to the save file

--- RCReminder/Settings.cs
+++ RCReminder/Settings.cs
@@ -16,6 +16,7 @@ namespace RCReminder
         public Boolean notify;
         public Boolean notifyViaEvery;
         public Boolean notifyViaAtTimes;
+        private String filePath = @"C:\Users\Mike\Desktop\RCREMINDER.SETTINGS";
 
         public Settings()
         {
@@ -75,23 +76,7 @@ namespace RCReminder
             //Make sure the hours =< 24 and minuts =< 60
             //--if not, don't save, keep values in fields, display modal popup
 
-            string[] fileContents = new string[13];
-
-            fileContents[0]  = this.remindEveryCheckBox.Checked.ToString();
-            fileContents[1]  = this.remindEveryPanel.Enabled.ToString();
-            fileContents[2]  = this.hoursTextBox.Text.ToString();
-            fileContents[3]  = this.minutesTextBox.Text.ToString();
-            fileContents[4]  = this.remindAtTimeCheckBox.Checked.ToString();
-            fileContents[5]  = this.remindAtTimePanel.Enabled.ToString();
-            fileContents[6]  = this.hoursComboBox1.SelectedItem.ToString();
-            fileContents[7]  = this.hoursComboBox2.SelectedItem.ToString();
-            fileContents[8]  = this.hoursComboBox3.SelectedItem.ToString();
-            fileContents[9]  = this.minutesComboBox1.SelectedItem.ToString();
-            fileContents[10] = this.minutesComboBox2.SelectedItem.ToString();
-            fileContents[11] = this.minutesComboBox3.SelectedItem.ToString();
-            fileContents[12] = this.notifyCheckBox.Checked.ToString();
-
-            File.WriteAllLines(@"C:\Users\Mike\Desktop\RCREMINDER.SETTINGS", fileContents);
+            SaveToFile(this.filePath);
         }
 
         private void cancelButton_Click(object sender, EventArgs e)
@@ -148,6 +133,208 @@ namespace RCReminder
         private void notifyCheckBox_CheckedChanged(object sender, EventArgs e)
         {
             this.notify = this.notifyCheckBox.Checked;
+        }
+
+        /// <summary>
+        /// Saves the users fields to a save file.
+        /// </summary>
+        /// <param name="fileLocation">The location of the file to save the values to.</param>
+        private void SaveToFile(String fileLocation)
+        {
+            string[] fileContents = new string[13];
+
+            fileContents[0] = this.remindEveryCheckBox.Checked.ToString();
+            fileContents[1] = this.remindEveryPanel.Enabled.ToString();
+            fileContents[2] = this.hoursTextBox.Text.ToString();
+            fileContents[3] = this.minutesTextBox.Text.ToString();
+            fileContents[4] = this.remindAtTimeCheckBox.Checked.ToString();
+            fileContents[5] = this.remindAtTimePanel.Enabled.ToString();
+            fileContents[6] = this.hoursComboBox1.SelectedItem.ToString();
+            fileContents[7] = this.hoursComboBox2.SelectedItem.ToString();
+            fileContents[8] = this.hoursComboBox3.SelectedItem.ToString();
+            fileContents[9] = this.minutesComboBox1.SelectedItem.ToString();
+            fileContents[10] = this.minutesComboBox2.SelectedItem.ToString();
+            fileContents[11] = this.minutesComboBox3.SelectedItem.ToString();
+            fileContents[12] = this.notifyCheckBox.Checked.ToString();
+
+            File.WriteAllLines(fileLocation, fileContents);
+        }
+
+        /// <summary>
+        /// Reads the saved data in from a file.
+        /// </summary>
+        /// <param name="fileLocation">The file location to read the data values from.</param>
+        private void ReadFromFile(String fileLocation)
+        {
+            try
+            {
+                string[] fileData = System.IO.File.ReadAllLines(fileLocation);
+
+                if (fileData.Length != 13)
+                {
+                    //Corrupt file... Reset file to defaults
+                    ResetSettingsFile(this.filePath);
+                }
+                else
+                {
+                    //Verify the data.  If the file was bad, re-create it
+                    if(!VerifyFields(fileData))
+                        ResetSettingsFile(fileLocation);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                //File could not be found. Re-create the file in the specified location.
+                ResetSettingsFile(this.filePath);
+            }
+            catch (FileLoadException)
+            {
+                //File could not load. Re-create the file in the specified location.
+                ResetSettingsFile(this.filePath);
+            }
+
+
+            //
+            // SAVE TO VARS
+            //
+
+
+        }
+
+        private void ResetSettingsFile(String fileLocation)
+        {
+            //Delete the file if it exists so we can re-create a new one.
+            if(File.Exists(fileLocation))
+            {
+                File.Delete(fileLocation);
+            }
+
+            string[] fileContents = new string[13];
+
+            fileContents[0] = true.ToString();
+            fileContents[1] = true.ToString();
+            fileContents[2] = "1";
+            fileContents[3] = "30";
+            fileContents[4] = false.ToString();
+            fileContents[5] = false.ToString();
+            fileContents[6] = " ";
+            fileContents[7] = " ";
+            fileContents[8] = " ";
+            fileContents[9] = " ";
+            fileContents[10] = " ";
+            fileContents[11] = " ";
+            fileContents[12] = true.ToString();
+
+            File.WriteAllLines(fileLocation, fileContents);
+        }
+
+        /// <summary>
+        /// Verifies that all of the fields read from the RCREMINDER.SETTINGS save file contain valid data.
+        /// </summary>
+        /// <param name="fileData">The data read in from the file</param>
+        /// <returns>A boolean value of if the file contains valid data.</returns>
+        private Boolean VerifyFields(string[] fileData)
+        {
+            int intgr;
+
+            if (!(fileData[0].ToString().ToUpper().Equals("TRUE") || fileData[0].ToString().ToUpper().Equals("FALSE")))
+                return false;
+
+            if (!(fileData[1].ToString().ToUpper().Equals("TRUE") || fileData[1].ToString().ToUpper().Equals("FALSE")))
+                return false;
+
+            if (int.TryParse(fileData[2], out intgr))
+            {
+                if (intgr > 24 || intgr < 0)
+                    return false;
+            }
+            else
+                return false;
+
+            if (int.TryParse(fileData[3], out intgr))
+            {
+                if (intgr > 60 || intgr < 0)
+                    return false;
+            }
+            else
+                return false;
+
+            if (!(fileData[4].ToString().ToUpper().Equals("TRUE") || fileData[4].ToString().ToUpper().Equals("FALSE")))
+                return false;
+
+            if (!(fileData[5].ToString().ToUpper().Equals("TRUE") || fileData[5].ToString().ToUpper().Equals("FALSE")))
+                return false;
+
+            if (!fileData[6].Equals(" "))
+            {
+                if (int.TryParse(fileData[6], out intgr))
+                {
+                    if (intgr > 23 || intgr < 0)
+                        return false;
+                }
+                else
+                    return false;
+            }
+
+            if (!fileData[7].Equals(" "))
+            {
+                if (int.TryParse(fileData[7], out intgr))
+                {
+                    if (!(intgr == 0 || intgr == 15 || intgr == 30 || intgr == 45))
+                        return false;
+                }
+                else
+                    return false;
+            }
+
+            if (!fileData[8].Equals(" "))
+            {
+                if (int.TryParse(fileData[8], out intgr))
+                {
+                    if (intgr > 59 || intgr < 0)
+                        return false;
+                }
+                else
+                    return false;
+            }
+
+            if (!fileData[9].Equals(" "))
+            {
+                if (int.TryParse(fileData[9], out intgr))
+                {
+                    if (!(intgr == 0 || intgr == 15 || intgr == 30 || intgr == 45))
+                        return false;
+                }
+                else
+                    return false;
+            }
+
+            if (!fileData[10].Equals(" "))
+            {
+                if (int.TryParse(fileData[10], out intgr))
+                {
+                    if (intgr > 23 || intgr < 0)
+                        return false;
+                }
+                else
+                    return false;
+            }
+
+            if (!fileData[11].Equals(" "))
+            {
+                if (int.TryParse(fileData[11], out intgr))
+                {
+                    if (!(intgr == 0 || intgr == 15 || intgr == 30 || intgr == 45))
+                        return false;
+                }
+                else
+                    return false;
+            }
+
+            if (!(fileData[1].ToString().ToUpper().Equals("TRUE") || fileData[1].ToString().ToUpper().Equals("FALSE")))
+                return false;
+
+            return true;
         }
     }
 }
